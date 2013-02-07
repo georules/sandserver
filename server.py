d98c7ffd0a33a7e055f030639e16dfbecfeb7704
Geoffery Miller <geoffery.miller@gmail.com>
2013-02-07 21:28:46 +0000

    json accept

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from flask import Flask, request
+from crossdomain import crossdomain
 import json,sys,os
 
 from straitjacket.lib import straitjacket
@@ -7,8 +8,8 @@ sjconfig = os.path.join(os.path.realpath(os.path.dirname(__file__)),"straitjacke
 print sjconfig
 sj = straitjacket.StraitJacket(sjconfig, False)
 
-def process(r):
-	stdout,stderr,exitstatus,runtime,error = sj.run("python",r.form['code'], "", None)
+def process(code):
+	stdout,stderr,exitstatus,runtime,error = sj.run("python",code, "", None)
 	result = json.dumps({"stdout":stdout,"stderr":stderr,"exitstatus":exitstatus,"time":runtime,"error":error})
 	return result
 
@@ -18,11 +19,23 @@ app = Flask(__name__)
 def root_response():
 	return "Heya<form method='post' action='/'><input type='text' name='code'/><input type='submit'/></form>"
 
-@app.route("/", methods=['POST'])
+@app.route("/", methods=['POST','OPTIONS'])
+@crossdomain(origin='*')
 def post_response():
-	out = process(request)
+	code = ""
+	if request.headers['Content-Type'] == 'application/json':
+		code = request.json['code']
+	else:
+		code = request.form['code']
+	out = process(code)
 	return out
+
+#@app.after_request
+#def after_request(response):
+#	response.headers.add('Access-Control-Allow-Origin', '*')
+#	response.headers.add('Content-Length',
+#	return response
 
 if __name__ == "__main__":
 	app.debug = True
-	app.run()
+	app.run("0.0.0.0",8080)
